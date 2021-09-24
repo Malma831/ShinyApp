5b1444464e67f6a07adc4f893824182399f3b172
Malma831 <89968327+Malma831@users.noreply.github.com>
2021-09-24 12:23:12 +0200

    test

---
+++ ShinyApp.R
@@ -0,0 +1,54 @@
+ui <- fluidPage(
+  titlePanel("Population in Linköping"),
+
+    mainPanel(
+    tabsetPanel(
+      tabPanel("Plot",
+        checkboxGroupInput(inputId = "Genders", label = "Select which gender to visualize: ", choices = c("Female" = "K", "Male" = "M", "Total" = "T")),
+               plotOutput(outputId = "plot", width = '75%', height = "700px")),
+
+      tabPanel("Tables", fluidRow(column(4, selectInput(inputId = "Year_var1", label="Select the first year", choices=1970:2020, width = '50%')),
+                             column(4, offset = 4, selectInput(inputId = "Year_var2", label="Select the second year", choices=1970:2020, width = '50%')),
+                fluidRow(column(4, tableOutput(outputId = "table1")), column(4, offset = 4, tableOutput(outputId = "table2"))
+
+                )
+
+
+    )
+  )
+)
+
+)
+)
+
+
+
+
+server <- function(input, output){
+
+  data <- reactive({
+    lab5Rpackage::linkoping_pop()
+  })
+  Year1<-reactive({
+    as.character(input$Year_var1)
+  })
+
+  txt_y1 <- renderText(as.character(input$Year_var1))
+
+  output$table1<-renderTable({data()[,c("gender","value")][data()$Year == input$Year_var1,]},
+                             caption.placement = "top", caption = "First year")
+
+
+
+  output$table2<-renderTable(data()[,c("gender","value")][data()$Year == input$Year_var2,],
+                             caption.placement = "top", caption = "Second year")
+
+  output$plot <- renderPlot({
+    ggplot(data()[data()$gender %in% input$Genders, ])+geom_line(aes(x=Year,y=value,color=gender))+theme_bw() + ylab("Population Count") +
+      labs(title = "Linkoping population by gender") + theme(plot.title = element_text(hjust = 0.5))
+  })
+}
+
+shinyApp(ui = ui, server = server)
+
+
